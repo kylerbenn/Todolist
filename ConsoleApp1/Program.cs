94c5d292ec83a096ee91b517af2ea2abfe488e3a
Kyler Benn <bennkr3@students.fscj.edu>
2018-07-24 14:20:16 -0400

    Worked on completing the project.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,133 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+
+            var todos = new List<string>();
+
+            var completedlist = new List<string>();
+           
+
+            Console.WriteLine("=============================================");
+
+                Console.WriteLine("TODO APP");
+
+                Console.WriteLine("=============================================");
+
+                Console.WriteLine(" Please enter your name:");
+
+                var name = Console.ReadLine();
+               
+                while (true)
+                {
+
+                    Console.WriteLine("=============================================");
+
+                    Console.WriteLine(" Choose an option " + name + " : ");
+
+                    Console.WriteLine("=============================================");
+                       
+                    Console.WriteLine(" 1. Add TODO ");
+
+                    Console.WriteLine(" 2. View TODOLIST ");
+
+                    Console.WriteLine(" 3. Remove a TODO from your TODOLIST ");
+
+                    Console.WriteLine(" 4. View Completed TODOLIST ");
+
+                    Console.WriteLine(" 5. QUIT ");
+                    
+                    string option = Console.ReadLine();
+
+
+
+                if (option == "1")
+                {
+                    Console.WriteLine("=============================================");
+
+                    Console.WriteLine("Add TODO:");
+
+                    Console.WriteLine("=============================================");
+
+                    string input = Console.ReadLine();
+
+                    todos.Add(input);
+
+                }
+                else if (option == "2")
+                {
+                    Console.WriteLine("=============================================");
+
+                    Console.WriteLine(" View TODOLIST:");
+
+                    Console.WriteLine("=============================================");
+
+                    for (var i=0; i <todos.Count; i++)
+                    {
+                        Console.WriteLine(todos[i]);
+                    }
+                }
+                else if (option == "3")
+                {
+                    Console.WriteLine("=============================================");
+
+                    Console.WriteLine(" Remove a TODO from your TODOLIST: ");
+
+                    Console.WriteLine("=============================================");
+
+                    for (var i=0; i <todos.Count; i++)
+                    {
+                        Console.WriteLine(i + todos[i]);
+                    }
+
+                    Console.WriteLine("=============================================");
+
+                    Console.WriteLine("Enter the number of the TODO you want to remove:");
+
+                    Console.WriteLine("=============================================");
+
+                    string input = Console.ReadLine();
+
+                    int choiceInt = int.Parse(input);
+
+                    string taskToMove = todos[choiceInt];
+
+                    todos.RemoveAt(choiceInt);
+
+                    completedlist.Add(input);
+
+                }
+
+                else if (option == "4")
+                {
+
+                    Console.WriteLine("=============================================");
+
+                    Console.WriteLine(" View Completed TODOLIST: ");
+
+                    Console.WriteLine("=============================================");
+
+                    for(var i=0; i<completedlist.Count; i++)
+                    {
+                        Console.WriteLine(completedlist[i]);
+                    }
+
+                }
+
+                else if (option == "5")
+                {
+                    Console.WriteLine("=============================================");
+
+                    Console.WriteLine(" QUIT ");
+
+                    Console.WriteLine("=============================================");
+
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Only enter 1, 2 , 3, 4, or 5");
+                }
+            }             
         }
     }
 }
